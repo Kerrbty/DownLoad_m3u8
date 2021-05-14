582108bf346472fd97f87feb3e5543edff3b7516
luguojie <jha334201553@163.com>
2021-05-14 19:43:56 +0800

    判断双引号

--- Release/ChangeFormat.bat
+++ Release/ChangeFormat.bat
@@ -2,7 +2,12 @@
 :next
 set filename=%1
 if '%filename%'=='' goto on_exit
+if "%filename:~-1%^"==""^" goto on_quotation
 ffmpeg.exe -i %filename% %filename:~0,-3%.mp4
+goto continue
+:on_quotation
+ffmpeg.exe -i %filename% %filename:~0,-4%.mp4"
+:continue
 del %filename%
 shift
 goto next
